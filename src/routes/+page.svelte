470d10b4544a8e35ae4fab55958fd1607c72d1f4
Niko Pastore <nikopastore@gmail.com>
2024-11-22 13:07:43 -0800

    asd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,37 @@
   import 'mapbox-gl/dist/mapbox-gl.css';
   import { onMount } from 'svelte';
 
-  // Your Mapbox access token (replace with your actual token)
   mapboxgl.accessToken = 'pk.eyJ1Ijoibmlrb3Bhc3RvcmUiLCJhIjoiY20zc2lpbzdoMGdqczJxcTB3czUzcTIxaiJ9.OxA2W627iC-9vWBa1_WHsg';
 
   let map;
 
   onMount(() => {
+    // Initialize the Mapbox map
     map = new mapboxgl.Map({
-      container: 'map', // ID of the div where the map will be rendered
-      style: 'mapbox://styles/mapbox/streets-v12', // Default Mapbox style
-      center: [-71.0942, 42.3601], // Longitude and Latitude (MIT's location)
-      zoom: 12, // Initial zoom level
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-71.0942, 42.3601],
+      zoom: 12,
+    });
+
+    // Add Boston bike lanes data source
+    map.on('load', () => {
+      map.addSource('boston_bike_lanes', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+      });
+
+      // Add a layer to visualize the bike lanes
+      map.addLayer({
+        id: 'boston_bike_lanes',
+        type: 'line',
+        source: 'boston_bike_lanes',
+        paint: {
+          'line-color': '#2ecc71', // Green color
+          'line-width': 3,
+          'line-opacity': 0.8,
+        },
+      });
     });
   });
 </script>
@@ -22,9 +42,9 @@
   @import url('$lib/global.css');
 
   #map {
-    width: 100%; /* Full width of the parent container */
-    height: 500px; /* Height of the map */
-    border: 1px solid #ccc; /* Optional: Add a border for clarity */
+    width: 100%;
+    height: 500px;
+    border: 1px solid #ccc;
   }
 </style>
 
